2933e6019a43d9a02c789a85c2f4f9f5b3a0f49d
xwfsdjk <743709284@qq.com>
2015-07-01 17:11:45 +0800

    nochange

--- test1.rb
+++ test1.rb
@@ -16,4 +16,48 @@ BEGIN {
 END {
     puts 'end ruby'
 }
+=begin
+this is comment
+type :
+1.integer
+2
+=end
+puts 'ssss"\\"'
+puts 'swa\'s'
+puts 'wanfa : #{1+2}'
+puts "wanfa : #{1+2}"
+puts '\a'
+ary = ['aa',1,2.1,"hahha",'lasrt']
+ary.each do |i|
+    puts i
+end
+hsh = colors = {'red'=>0xf00,'green'=>0x0f0,'blue'=>0x00f}
+hsh.each do |key,value|
+    print key , 'is ', value , "\n"
+end
+(10..15).each do |n|
+    print n,' '
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
